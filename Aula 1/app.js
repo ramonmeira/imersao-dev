009c58d8235318f22a07f176812d6cc7e0edce09
Ramon Lima e Meira <ramonlimaemeira@gmail.com>
2021-09-24 15:32:31 -0300

    Desenvolvimento durante Aula 1

---
+++ Aula 1/app.js
@@ -0,0 +1,32 @@
+var name = 'Guilerme';
+var firstQuarterGrade = 9.4335;
+var secondQuarterGrade = 7.234;
+var thirdQuarterGrade = 4.234;
+var fourthQuarterGrade = 2.183;
+
+var finalGrade =
+  (firstQuarterGrade +
+    secondQuarterGrade +
+    thirdQuarterGrade +
+    fourthQuarterGrade) /
+  4;
+
+var fixedFinalGrade = finalGrade.toFixed(1);
+
+//console.log("Bem vindo " + name)
+console.log(fixedFinalGrade);
+
+//Revisão
+// Variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
+
+//Desafios
+
+// 1- Dependendo da nota, mostrar se a aluna ou aluno foi aprovada(o) ou não
+
+// 2 - Alterar o fundo da tela da maneira que achar mais legal
+
+// 3 - Imprimir na própria página o resultado, ao invés do console
+
+// 4 - Criar um conversor de temperaturas entre farenheit e celcius
+
+// 5 - Colocar a conta inteira da média em apenas uma linha
