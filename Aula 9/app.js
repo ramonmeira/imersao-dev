1264825fbb93a1855e2f9403c7ed64af758adde2
Ramon Lima e Meira <ramonlimaemeira@gmail.com>
2021-10-04 14:24:33 -0300

    Aula 9: Código desenvolvido na aula

---
+++ Aula 9/app.js
@@ -0,0 +1,4 @@
+// Terminar todo o design que está no figma (disponível abaixo)
+// https://www.figma.com/file/1flmz2iauuNs8JsY6eaBHI/Imers%C3%A3o-Dev---Aula-9
+
+// Adicionar suas redes sociais de forma que quem clique consiga abrí-las
